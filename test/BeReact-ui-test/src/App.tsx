a977a67dd5a428fea6f67b46c9037c699e90970e
Nathan Lombardelli <nathanlombardelli@hotmail.com>
2024-03-20 19:02:41 +0100

    solution prop --x

--- test/BeReact-ui-test/src/App.tsx
+++ test/BeReact-ui-test/src/App.tsx
@@ -12,10 +12,10 @@ function App() {
           {/* TESTS ZONE  */}
 
           <div style={{display:"flex"}}>
-            <HoloCard size={550} pattern={"../img/rareEffect1.jpg"} content={<img src="../img/CashAppCard.jpg" alt=""/>}/>
+            <HoloCard beginWithEffect endKeepEffect size={650} pattern={"../img/rareEffect1.jpg"} content={<img src="../img/CashAppCard.jpg" alt=""/>}/>
             <div style={{display:"flex",flexDirection:"column",gap:'50px'}}>
-                <HoloCard size={250} beginWithEffect pattern={"../img/rareEffect1.jpg"} content={<img src="../img/bgMoon.jpg" alt=""/>}/>
-                <HoloCard size={250} beginWithEffect endKeepEffect pattern={"../img/rareEffect1.jpg"} content={<img src="../img/bgMoon.jpg" alt=""/>}/>
+                <HoloCard size={50} beginWithEffect pattern={"../img/rareEffect1.jpg"} content={<img src="../img/bgMoon.jpg" alt=""/>}/>
+                <HoloCard size={50} beginWithEffect endKeepEffect pattern={"../img/rareEffect1.jpg"} content={<img src="../img/bgMoon.jpg" alt=""/>}/>
             </div>
               {/*<HoloCard pattern={"../img/rareEffect1.jpg"} content={<p style={{backgroundColor:'red'}}>Test</p>}/>*/}
           </div>
